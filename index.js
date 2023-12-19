26061f839d43d1db9d23f65be880aac32b670365
rahulhasan31 <rahulhasan666@gmail.com>
2023-12-19 22:25:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io'); // Add this
+
+app.use(cors()); // Add cors middleware
+
+const server = http.createServer(app); // Add this
+
+// Add this
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
+
+// Add this
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  socket.on("send-message", (message)=>{
+    console.log(message);
+
+    io.emit("received-message", message)
+  })
+
+  // We can write our socket event listeners in here...
+  socket.on("disconnect" , ()=>{
+    console.log('user disconnect');
+  })
+});
+
+server.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
